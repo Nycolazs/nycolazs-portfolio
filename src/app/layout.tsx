032a20debd5abefc3f7032e89919bfbd84da582a
Nycolas <pedro.nycolas.2003@gmail.com>
2026-02-12 12:52:36 -0300

    chore: update project configuration and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Space_Grotesk } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import Script from "next/script";
 import "./globals.css";
 
